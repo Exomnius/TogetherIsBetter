20d53a911ec7fc7c8be86403dc07fec19d2f1ae0
Exmonius <janssen_cornel@hotmail.com>
2014-05-08 14:26:48 +0200

    Added Calendar + overview layout

--- Program/TogetherIsBetter/TogetherIsBetter/overview.xaml.cs
+++ Program/TogetherIsBetter/TogetherIsBetter/overview.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MonthCalendar;
 
 namespace TogetherIsBetter
 {
@@ -19,9 +20,52 @@ namespace TogetherIsBetter
     /// </summary>
     public partial class overview : Window
     {
+        private List<Appointment> _myAppointmentsList = new List<Appointment>();
+
         public overview()
         {
             InitializeComponent();
+            Loaded += Window1_Loaded;
+        }
+
+        private void Window1_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Random rand = new Random(System.DateTime.Now.Second);
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Appointment apt = new Appointment();
+                apt.AppointmentID = i;
+                apt.StartTime = new System.DateTime(System.DateTime.Now.Year, 5, i);
+                apt.EndTime = apt.StartTime;
+                apt.Subject = "Random apt, blah blah";
+                _myAppointmentsList.Add(apt);
+            }
+
+            SetAppointments();
+        }
+
+        private void DayBoxDoubleClicked_event(NewAppointmentEventArgs e)
+        {
+            MessageBox.Show("You double-clicked on day " + Convert.ToDateTime(e.StartDate).ToShortDateString(), "Calendar Event", MessageBoxButton.OK);
+        }
+
+        private void AppointmentDblClicked(int Appointment_Id)
+        {
+            MessageBox.Show("You double-clicked on appointment with ID = " + Appointment_Id, "Calendar Event", MessageBoxButton.OK);
+        }
+
+        private void DisplayMonthChanged(MonthChangedEventArgs e)
+        {
+            SetAppointments();
+        }
+
+        private void SetAppointments()
+        {
+            //-- Use whatever function you want to load the MonthAppointments list, I happen to have a list filled by linq that has
+            //   many (possibly the past several years) of them loaded, so i filter to only pass the ones showing up in the displayed
+            //   month.  Note that the "setter" for MonthAppointments also triggers a redraw of the display.
+            AptCalendar.MonthAppointments = _myAppointmentsList.FindAll(new System.Predicate<Appointment>((Appointment apt) => apt.StartTime != null && Convert.ToDateTime(apt.StartTime).Month == this.AptCalendar.DisplayStartDate.Month && Convert.ToDateTime(apt.StartTime).Year == this.AptCalendar.DisplayStartDate.Year));
         }
     }
 }
