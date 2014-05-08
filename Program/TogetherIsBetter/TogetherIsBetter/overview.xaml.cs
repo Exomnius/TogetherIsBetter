99ef9e5d9652a1c80dc0249ba5afad5974c4c123
Exmonius <janssen_cornel@hotmail.com>
2014-05-08 11:19:50 +0200

    Started overview screen

---
+++ Program/TogetherIsBetter/TogetherIsBetter/overview.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TogetherIsBetter
+{
+    /// <summary>
+    /// Interaction logic for overview.xaml
+    /// </summary>
+    public partial class overview : Window
+    {
+        public overview()
+        {
+            InitializeComponent();
+        }
+    }
+}
