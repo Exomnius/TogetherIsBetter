5024e98ed16ffc672d23e091c10bca4e1a395efa
Exmonius <janssen_cornel@hotmail.com>
2014-05-09 22:41:34 +0200

    Recommit after failed merge

--- Program/TogetherIsBetter/MonthCalendar/MonthCalendar.xaml.cs
+++ Program/TogetherIsBetter/MonthCalendar/MonthCalendar.xaml.cs
@@ -19,49 +19,9 @@ namespace MonthCalendar
     /// </summary>
     public partial class UserControl1 : UserControl
     {
-<<<<<<< HEAD:Program/TogetherIsBetter/MonthCalendar/MonthCalendar.xaml.cs
         public UserControl1()
-=======
-        private TIB_Model db;
-        public MainWindow()
->>>>>>> remotes/origin/master:Program/TogetherIsBetter/TogetherIsBetter/MainWindow.xaml.cs
         {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            using (db = new TIB_Model()){
-                //get all objects
-                var query = from b in db.Company
-                            orderby b.Name 
-                            select b;
-                
-                List<Company> companies = new List<Company>();
-                foreach (var item in query)
-                {
-                    companies.Add(item);
-                } 
-
-               test1.Text = companies[1].Name;
-
-                //lookup by key
-               Location loc = db.Location.Find(1);
-               test2.Text = loc.Name;
-
-                //lookup by where
-               Location loca = db.Location.Where(b => b.Name.Equals("Room 1")).FirstOrDefault();
-               test3.Text = loca.Name;
-                
-                //all objects in table
-               List<Reservation> reserves = new List<Reservation>();
-               reserves = db.Reservation.ToList();
-
-               foreach (Reservation r in reserves)
-               {
-                   testlijst.Items.Add(r.Id + "" + r.Company.Name + r.StartDate.ToString());
-               }
-            }
         }
     }
 }
