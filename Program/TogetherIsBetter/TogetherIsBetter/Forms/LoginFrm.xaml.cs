e2de6cd13a17e3acbca027327ba4efbc3d64fa64
arno van waeyenberg <arnovanwaeyenberg@gmail.com>
2014-05-10 14:20:09 +0200

    Merge of Arno/Nick/Cornel

---
+++ Program/TogetherIsBetter/TogetherIsBetter/Forms/LoginFrm.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Web.Security;
+
+namespace TogetherIsBetter
+{
+    /// <summary>
+    /// Interaction logic for LoginForm.xaml
+    /// </summary>
+    public partial class LoginForm : Window
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            String username, password;
+
+            username = tbUsername.Text;
+            password = tbPassword.Password;
+
+            if (Membership.ValidateUser(username, password))
+            {
+                MessageBox.Show("GELDIG!");
+            }
+            else
+            {
+                MessageBox.Show("Ongeldige gebruikersnaam en/of wachtwoord.");
+            }
+
+
+        }
+    }
+}
